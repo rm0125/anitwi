c8a9f282c33bbf35e9f4c3761ff01d2b63acc1e5
Shouhei Umemoto <shouhei.umemoto@gmail.com>
2013-11-21 20:36:10 +0900

    タイムラインのツイートを取得

--- anitwi/src/AniTwi.java
+++ anitwi/src/AniTwi.java
@@ -1,4 +1,20 @@
+import java.util.List;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 
 public class AniTwi {
+
+	public static void main(String[] args) throws TwitterException {
+		Twitter twitter = TwitterFactory.getSingleton();
+		List<Status> statuses = twitter.getHomeTimeline();
+		System.out.println("Showing home timeline.");
+		for (Status status : statuses) {
+			System.out.println(status.getUser().getName() + ":"
+					+ status.getText());
+		}
+	}
 
 }
