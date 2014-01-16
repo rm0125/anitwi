e0dfa219433bddeb140c895c21dc7cbc3345da13
t-takahashi <t-takahashi@atware.co.jp>
2014-01-16 19:38:54 +0900

    整形

--- anitwi/src/AniTwi.java
+++ anitwi/src/AniTwi.java
@@ -9,22 +9,17 @@ import twitter4j.TwitterFactory;
 public class AniTwi {
 
 	public static void main(String[] args) throws TwitterException {
-
 		Twitter twitter = TwitterFactory.getSingleton();
 		List<Status> statuses = twitter.getHomeTimeline();
 		System.out.println("Showing home timeline.");
 		for (Status status : statuses) {
-			System.out.println(status.getUser().getName() + ":"
-					+ status.getText());
+			System.out.println(status.getUser().getName() + ":" + status.getText());
 		}
-
 
 	    // このファクトリインスタンスは再利用可能でスレッドセーフです
 	    Twitter sender = TwitterFactory.getSingleton();
 	    DirectMessage message = sender.sendDirectMessage(sender.getId(), "abc");
 	    System.out.println("Sent: " + message.getText() + " to @" + message.getRecipientScreenName());
-
-
 	}
 
 }
