8f19a5f21267baace115d36966a87c84b656fcd4
Junko <Junko@192.168.20.16>
2014-07-10 20:58:11 +0900

    tweet部分実装

--- anitwi/src/jp/hakoika/study/anitwi/AnitwiMain.java
+++ anitwi/src/jp/hakoika/study/anitwi/AnitwiMain.java
@@ -1,5 +1,7 @@
 package jp.hakoika.study.anitwi;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import jp.hakoika.study.anitwi.animeinfo.AnimeInfoLogic;
@@ -7,6 +9,11 @@ import jp.hakoika.study.anitwi.animeinfo.AnimeInfoModel;
 import jp.hakoika.study.anitwi.client.HttpClient;
 
 import org.w3c.dom.Document;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 
 public class AnitwiMain {
 
@@ -17,11 +24,30 @@ public class AnitwiMain {
 
 		String url = "http://animemap.net/api/table/hokkaidou.xml";
 
+		Date current = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+		String todayStr = sdf.format(current);
+
 		HttpClient client = new HttpClient();
 		Document document = client.getXml(url);
 
 		AnimeInfoLogic logic = new AnimeInfoLogic();
 		List<AnimeInfoModel> animeInfoList = logic.createAnimeInfo(document);
 
+		try {
+			Twitter twitter = TwitterFactory.getSingleton();
+
+			for (AnimeInfoModel animeInfo : animeInfoList) {
+				String tweet = animeInfo.getTitle() + "\n";
+				tweet += todayStr + " " +animeInfo.getTime() + "放送開始（" + animeInfo.getStation() + "）";
+				Status status = twitter.updateStatus(tweet);
+				System.out.println("Successfully updated the status to [" + status.getText() + "].");
+			}
+
+			twitter.updateStatus("tweet test");
+		} catch (TwitterException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 	}
 }
